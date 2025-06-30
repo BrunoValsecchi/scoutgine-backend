5af28508d637e02d753f6c4f8613301d1403aa9e
Bruno Valsecchi <cai.bruno.valsecchi@gmail.com>
2025-06-30 20:55:19 -0300

    Fix wsgi.py and vercel.json - force redeploy 20:55:19

--- scoutgine/wsgi.py
+++ scoutgine/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scoutgine.settings')
 
 application = get_wsgi_application()
+
+# Para Vercel - crear alias de la variable application
+app = application
+handler = application
