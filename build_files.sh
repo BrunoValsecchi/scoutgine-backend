c68288b4bca5f77eb70a53d7438488e948ee6ee0
Bruno Valsecchi <cai.bruno.valsecchi@gmail.com>
2025-07-01 00:09:01 -0300

    Fix Python commands in build script

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Building the project..."
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 echo "Make Migration..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 echo "Collect Static..."
-python manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
