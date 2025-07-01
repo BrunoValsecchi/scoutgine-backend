1c3922d251993ba2603a1a79065f0764589b7790
Bruno Valsecchi <cai.bruno.valsecchi@gmail.com>
2025-06-30 21:20:19 -0300

    Clean repo for new Vercel project

--- build_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "BUILD START"
-echo "Python version: $(python3 --version)"
-pip3 install --upgrade pip
-pip3 install -r requirements.txt
-mkdir -p staticfiles_build
-python3 manage.py collectstatic --noinput --clear
-echo "BUILD END"
